6c70225401707dac307e4e0bf33115c8fb0758cd
AliceAA <39633945+AliceAA@users.noreply.github.com>
2021-01-14 11:07:40 +0200

    Check date

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
 import requests
 import pandas as pd
-import json
-import time
-from flask import Flask, request
-import pymongo
+from flask import Flask
 from datetime import datetime
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
 
-@app.route("/")#, methods=['POST']
+@app.route("/new")#, methods=['POST']
 def echo():
     #data = request.json
     data = {
       "user" : "username",
       "date": {
-        "timestamp": "12324325245",
-        "range": 40000
+        "timestamp": "1601840340",
+        "range": 100000
       },
       "location": {
         "lat": 39.5,
@@ -38,13 +35,17 @@ def echo():
     lon_min = data['location']['lon']-data['location']['range']
     lon_max = data['location']['lon']+data['location']['range']
 
+    date_min = datetime.fromtimestamp(int(data['date']['timestamp'])-data['date']['range'])
+    date_max = datetime.fromtimestamp(int(data['date']['timestamp'])+data['date']['range'])
+
     answer = list(collection.find({"$and": [{'geometry.0.coordinates.0': {'$gt': lat_min, '$lt': lat_max}},
                                          {'geometry.0.coordinates.1': {'$gt': lon_min, '$lt': lon_max}}]}))
+    #,{'geometry.0.date': {'$gt': date_min, '$lt': date_max}}
     answer = {"events": [form_answer(i) for i in answer]}
     print(answer)
-    return "OK"
+    return "ok"
 
-
+# 2020-10-04T22:39:00Z
 def form_answer(item):
     return {"description": item['title'],
                  "date": datetime.timestamp(pd.to_datetime(item['geometry'][0]['date'])),
