eeb5b36ce79fa51a96af1188b08ea7deaa23cb23
AliceAA <39633945+AliceAA@users.noreply.github.com>
2021-01-23 13:51:40 +0200

    Functional style

--- disaster-service/src/update_db.py
+++ disaster-service/src/update_db.py
@@ -1,51 +1,67 @@
 import requests
-import time
-import json
 from datetime import datetime
 import pandas as pd
 from pymongo import MongoClient
+import time
+import json
+import asyncio
+import rx
+import rx.operators as ops
+from rx.scheduler.eventloop import AsyncIOScheduler
+import logging
 from confluent_kafka import Producer
 import os
 import socket
+
 
 conf = {'bootstrap.servers': os.environ['KAFKA_HOST'],
         'client.id': socket.gethostname()}
 producer = Producer(conf)
 key_counter = 1
 
+
 def send_notification(event):
-    print(event)
     global key_counter
     if len(event['geometry']) > 0 and len(event['geometry'][0]['coordinates']) > 1:
         notification = {
             "disaster": {"description": event['title'],
-                     "date": int(datetime.timestamp(pd.to_datetime(event['geometry'][0]['date']))) * 1000,
+                     "date": datetime.timestamp(pd.to_datetime(event['geometry'][0]['date'])),
                      "lat": event['geometry'][0]['coordinates'][0],
                      "lon": event['geometry'][0]['coordinates'][1]}
         }
         print(notification)
-        producer.produce(os.environ['KAFKA_TOPIC'], key=str(++key_counter), value=bytes(json.dumps(notification), 'utf-8'))
+        producer.produce(os.environ['KAFKA_TOPIC'], key=str(++key_counter),
+                         value=bytes(json.dumps(notification), 'utf-8'))
+    return event
 
 
-def get_events():
-    conn = MongoClient(os.environ['MONGO_HOST'], username=os.environ['MONGO_USER'], password=os.environ['MONGO_PASS'])
+async def foo():
+    print("Next interval")
+    return requests.get("https://eonet.sci.gsfc.nasa.gov/api/v3/events").json()['events']
+
+
+def intervalRead(rate, fun, collection) -> rx.Observable:
+    loop = asyncio.get_event_loop()
+    return rx.interval(rate).pipe(
+        ops.map(lambda i: rx.from_future(loop.create_task(fun()))),
+        ops.merge_all(),
+        ops.map(lambda i: list(filter(lambda j: collection.find_one({'id': j['id']}) is None, i))),
+        ops.map(lambda i: [send_notification(j) for j in i])
+    )
+
+
+async def main(loop):
+    conn = MongoClient()
     print("Connected successfully!!!")
 
-    db = conn.disaster # create database
-    collection = db.events # create connection
-
-    while True:
-        response = requests.get("https://eonet.sci.gsfc.nasa.gov/api/v3/events").json()['events']
-
-        print(f"Got {len(response)} events from api")
-        # save to mongo
-        for event in response:
-            if collection.find_one({'id':  event['id']}) is None:
-                collection.insert_one(event)
-                send_notification(event)
-
-        time.sleep(60)  # Sleep for 60 seconds
+    collection = conn.disaster.events
+    obs = intervalRead(3, foo, collection)
+    obs.subscribe(
+        on_next=lambda i: [collection.insert_one(j) for j in i],
+        scheduler=AsyncIOScheduler(loop)
+    )
 
 
-if __name__ == "__main__":
-    get_events()
+loop = asyncio.get_event_loop()
+loop.create_task(main(loop))
+loop.run_forever()
